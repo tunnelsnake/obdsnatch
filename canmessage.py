151e479f619bf4fa7cf83bcc36bf485b571e5dc7
Jacob Thomas <baconbattle@gmail.com>
2018-03-28 13:39:50 -0400

    fuckin bugs man

--- canmessage.py
+++ canmessage.py
@@ -3,7 +3,7 @@
 class CanMessage:
 
     def __init__(self, cob_id, data, recv_flag):
-        self.cob_id = int(cob_id, 16)
+        self.cob_id = cob_id
         self.datalen = len(str((data)))
         self.data = data
         self.recv_flag = recv_flag
