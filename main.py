10bf81748dbf5faf574542cee1f0ee9553c0b0d8
Jacob Thomas <baconbattle@gmail.com>
2018-03-28 14:51:35 -0400

    fuckin bugs man

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class OBDSnatch:
 
     def __init__(self):
         self.rbus = cs.CANSocket("can0")
-        self.fbus = cs.CANSocket("can1")
+        #self.fbus = cs.CANSocket("can1")
         print("Starting OBDSnatch...")
 
     def start(self):
@@ -14,13 +14,13 @@ class OBDSnatch:
             rbus_message = self.rbus.recv();
             if rbus_message == None:
                 print("RBUS MESSAGE NULL")
-                fbus_message = self.fbus.recv();
-                print("FBUS Message NULL")
+                #fbus_message = self.fbus.recv();
+                #print("FBUS Message NULL")
 
             if rbus_message != None:
-                self.fbus.send(rbus_message)
-            if fbus_message.recv_flag != None:
-                self.rbus.send(fbus_message)
+                self.rbus.send(rbus_message)
+            #if fbus_message != None:
+                #self.rbus.send(fbus_message)
 
 o = OBDSnatch()
 o.start()
