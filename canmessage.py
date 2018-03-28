84d68aef044542d417d29e931694331aabd1ca81
Jacob Thomas <baconbattle@gmail.com>
2018-03-28 13:15:37 -0400

    abcdefghijklmnopqr

--- canmessage.py
+++ canmessage.py
@@ -3,9 +3,5 @@
 class CanMessage:
 
     def __init__(self, cob_id, data):
-        if (len(str(cob_id)) == 3):
-            self.cob_id = cob_id
-        else:
-            self.cob_id = -1
         self.datalen = len(str((data)))
         self.data = data
