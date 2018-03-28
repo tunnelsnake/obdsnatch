000e9a6e916515fed4636ec234e293bd071bd02b
Jacob Thomas <baconbattle@gmail.com>
2018-03-28 14:21:15 -0400

    fuckin bugs man

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ class OBDSnatch:
 
         while(True):
             rbus_message = self.rbus.recv();
+            if rbus_message == None:
+                print("RBUS MESSAGE NULL")
             fbus_message = self.fbus.recv();
+                print("FBUS Message NULL")
 
             if rbus_message != None:
                 self.fbus.send(rbus_message)
