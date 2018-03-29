46b44d99dcbd53bb030cc900a9a14a15e1980c43
Jacob Thomas <baconbattle@gmail.com>
2018-03-28 23:12:50 -0400

    fuckin bugs man

--- canmessage.py
+++ canmessage.py
@@ -2,15 +2,15 @@ import binascii
 import struct
 
 class CanMessage:
+    debug = True;
 
     def __init__(self, cob_id, data, recv_flag):
         self.cob_id = cob_id
         self.datalen = len(data)
         self.data = data
         self.recv_flag = recv_flag
-
-        print("MESSAGE CREATED")
-        print("COB ID: %03x" % self.cob_id)
-        print("DATALEN: " + str(self.datalen))
-        #print("DATA: " + data)
-        print("RECV FLAG: " + str(recv_flag))
+        if self.debug:
+            print("MESSAGE CREATED")
+            print("COB ID: %03x" % self.cob_id)
+            print("DATALEN: " + str(self.datalength))
+            print("RECV FLAG: " + str(recv_flag))
--- main.py
+++ main.py
@@ -5,22 +5,19 @@ class OBDSnatch:
 
     def __init__(self):
         self.rbus = cs.CANSocket("can1")
-        #self.fbus = cs.CANSocket("can1")
+        self.fbus = cs.CANSocket("can1")
         print("Starting OBDSnatch...")
 
     def start(self):
 
         while(True):
             rbus_message = self.rbus.recv();
-            if rbus_message == None:
-                print("RBUS MESSAGE NULL")
-                #fbus_message = self.fbus.recv();
-                #print("FBUS Message NULL")
+            fbus_message = self.fbus.recv();
 
             if rbus_message != None:
-                self.rbus.send(rbus_message)
-            #if fbus_message != None:
-                #self.rbus.send(fbus_message)
+                self.fbus.send(rbus_message)
+            if fbus_message != None:
+                self.rbus.send(fbus_message)
 
 o = OBDSnatch()
 o.start()
