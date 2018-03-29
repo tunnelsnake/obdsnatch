2913292712c08b2b9d259c089848f03be0e8920b
Jacob Thomas <baconbattle@gmail.com>
2018-03-29 15:16:45 -0400

    fuckin bugs man

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ class OBDSnatch:
 
     def start(self):
         while(True):
-            rbus_message = self.rbus.recv()
+            #rbus_message = self.rbus.recv()
             fbus_message = self.fbus.recv()
 
-            if rbus_message != None:
-                if rbus_message.cob_id >= 0x7E7:
-                    self.fbus.send(rbus_message)
+            #if rbus_message != None:
+            #    if rbus_message.cob_id >= 0x7E7:
+            #        self.fbus.send(rbus_message)
             if fbus_message != None:
                 if fbus_message.cob_id == 0x7DF:
                     self.rbus.send(fbus_message)
