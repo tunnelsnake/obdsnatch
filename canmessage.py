f2d8c0e6efbcbac1a26c773de2e7f6f4f950d15a
Jacob Thomas <baconbattle@gmail.com>
2018-03-30 12:45:16 -0400

    added logging facillity

--- canmessage.py
+++ canmessage.py
@@ -6,12 +6,13 @@ class CanMessage:
 
     def __init__(self, cob_id, data, recv_flag=True):
         self.cob_id = cob_id
-        self.datalen = len(data)
         self.data = data
+        self.datalen = len(data)
         self.recv_flag = recv_flag
         if self.debug:
             print("MESSAGE CREATED")
             print("COB ID: %03x" % self.cob_id)
+            print("DATALEN: " + str(self.datalen))
             print("RECV FLAG: " + str(recv_flag))
 
     def getmode(self):
