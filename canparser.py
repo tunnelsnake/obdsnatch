36d0e35411a9e9b48000a7a5dae348c89cf0ec96
Jacob Thomas <baconbattle@gmail.com>
2018-04-03 19:51:51 -0400

    a

--- canparser.py
+++ canparser.py
@@ -15,5 +15,8 @@ class CanParser:
         if message.getbyte(0) == 0x02 and message.getbyte(1) == 0x01 and message.getbyte(2) == 0x01:  #Mode 02 PID 01 (Information Header)
             self.logger.info("[+] Mode 02 PID 01 Intercepted.")
             self.fbus.send(cm.CanMessage(0x7e8, b"\x06\x41\x01\x00\x07\xed\x00\x00"))
+        elif message.getbyte(0) == 0x01 and message.getbyte(1) == 0x07: #Mode 07 - Pending Codes
+            self.logger.info("[+] Pending Code Read Intercepted.")
+            self.fbus.send(cm.CanMessage(0x7e8, b"\x00\x00\x00\x00\x00\x00\x00\x00"))
         else:
             self.rbus.send(message) #send the real message over the real bus
