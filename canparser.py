de295cfcdb18d52248167ecefb6ad98e4f9fd806
Jacob Thomas <baconbattle@gmail.com>
2018-04-03 20:05:12 -0400

    a

--- canparser.py
+++ canparser.py
@@ -18,5 +18,8 @@ class CanParser:
         elif message.getbyte(0) == 0x01 and message.getbyte(1) == 0x07: #Mode 07 - Pending Codes
             self.logger.info("[+] Pending Code Read Intercepted.")
             self.fbus.send(cm.CanMessage(0x7e8, b"\x00\x00\x00\x00\x00\x00\x00\x00"))
+        elif message.getbyte(0) == 0x02 and message.getbyte(1) == 0x01 and message.getbyte(2) == 0x0D:
+            self.logger.info("[+] Speed Code Intercepted")
+            self.fbus.send(cm.CanMessage(0x7e8, b"\x03\x41\x0d\x55\x55\x00\x00\x00"))
         else:
             self.rbus.send(message) #send the real message over the real bus
