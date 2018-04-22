a88befb030de01c8964128d796271390101864ec
Jacob Thomas <baconbattle@gmail.com>
2018-04-22 18:11:30 -0400

    debugging profiler

--- canprofile.py
+++ canprofile.py
@@ -52,7 +52,7 @@ class CanProfile:
                 response_message = self.rbus.recv()
                 if response_message is not None and 0x7e8 <= response_message.cob_id <= 0x7ef:
                     cob_id_list.append(response_message)
-                millis = calendar.timegm(time.gmtime())
+                    millis = calendar.timegm(time.gmtime())
             if len(cob_id_list) > 0:
                 cob_id_list = list(set(cob_id_list))
                 self.logger.info("[+] Query Number " + num + " Returned " + len(cob_id_list) + " Results.")
